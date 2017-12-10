43c782127c69bf6766b323875ed49889b06282db
horsekitlin <horsekit1982@gmail.com>
2017-12-10 10:21:42 +0800

    調整 facebook develop api id

--- public/js/facebook-connect.js
+++ public/js/facebook-connect.js
@@ -1,19 +1,18 @@
 /**
  * facebook-connect.js
  */
- 
+
 (function(){
   var fbook = {};
 
   // Create app object
   var Fbook = function(){
     this.config = {
-        appId:     '551212248226405',
+        appId:     '374881026291601',
         status:     true, // check login status
         cookie:     true, // enable cookies to allow the server to access the session
         xfbml:      true, // parse XFBML
-        oauth:      true, // enable OAth 2.0
-        channelUrl: 'http://taiwan-up-up.nko3.jit.su/js/channel.html'
+        version: "v2.8"
     }
     this.$login = $('#login-btn');
   };
@@ -94,64 +93,3 @@
 
 
 
-
- 
-/*
-$(function() {
-	
-	FB.init({
-		appId:     '551212248226405',
-		status:     true, // check login status
-		cookie:     true, // enable cookies to allow the server to access the session
-		xfbml:      true, // parse XFBML
-		oauth:      true, // enable OAth 2.0
-		channelUrl: 'http://taiwan-up-up.nko3.jit.su/js/channel.html'
-	});
-	
-	$('#login-btn').click(function() {
-		
-		var permissions = 'email,' +
-		                  'user_birthday,' +
-		                  'user_education_history,' +
-		                  'user_groups,' +
-		                  'user_hometown,' +
-		                  'user_interests,' +
-		                  'user_likes,' +
-		                  'user_relationship_details,' +
-		                  'user_subscriptions,' +
-		                  'publish_actions,' +
-		                  'manage_friendlists,' +
-		                  'publish_stream,' +
-		                  'offline_access,' +
-		                  'manage_pages';
-		
-		var callbackLogin = function(response) {
-			
-			//console.log(response);
-			
-			if (response.status != 'connected' || response.authResponse == null) {
-				
-				console.log('not connected');
-				return;
-			}
-			var userId = response.authResponse.userID;
-			var token = response.authResponse.accessToken;
-			
-			var message = 'userId = ' + userId + '\n' +
-			              'accessToken = ' + token.substr(0, 20) + '...' + '\n\n' +
-			              'write them back through api';
-			FB.api('/me', function(response) {
-				//console.log(response);
-        startGame(response);
-			});
-		};
-		
-		//FB.login(callbackLogin, permissions);
-    //FB.getLoginStatus(callbackLogin, true);
-		FB.login(function(){
-      FB.getLoginStatus(callbackLogin, true);
-    }, permissions);
-
-	});
-});
-*/
--- routes/index.js
+++ routes/index.js
@@ -2,93 +2,84 @@
  * GET home page.
  */
 
-var utils    = require( 'connect' ).utils;
-
+var utils = require("connect").utils;
 
 //  Main Page
-exports.index = function ( req, res, next ){
-    //~ var params = { title : 'OSSII Chat' };
-//~ 
-    //~ //  use request.session.isLogin to check login
-    //~ if( req.session.isLogin && req.session.isLogin!==''){
-            //~ params.isLogin = true;
-            //~ params.user = req.session.user;
-    //~ }else{
-        //~ params.isLogin = false;
-    //~ }
-    res.render( 'index' );
+exports.index = function(req, res, next) {
+  //~ var params = { title : 'OSSII Chat' };
+  //~
+  //~ //  use request.session.isLogin to check login
+  //~ if( req.session.isLogin && req.session.isLogin!==''){
+  //~ params.isLogin = true;
+  //~ params.user = req.session.user;
+  //~ }else{
+  //~ params.isLogin = false;
+  //~ }
+  res.render("index");
 };
 
-exports.room = function ( req, res, next ){
-    res.render( 'room' );
-    //~ res.sendfile(__dirname + '../public/room.html');
-}
+exports.room = function(req, res, next) {
+  res.render("room");
+  //~ res.sendfile(__dirname + '../public/room.html');
+};
 
-exports.room2 = function ( req, res, next ){
-  console.log('ccccccccccc');
-  if (! req.params.hasOwnProperty("room")) {
+exports.room2 = function(req, res, next) {
+  if (!req.params.hasOwnProperty("room")) {
     res.redirect("/");
   }
 
-  Todo.
-    find({ room_id : req.params.room }).
-    sort( 'updated_at', -1 ).
-    run( function ( err, todos ){
-      if( err ) return next( err );
+  Todo.find({ room_id: req.params.room })
+    .sort("updated_at", -1)
+    .run(function(err, todos) {
+      if (err) return next(err);
 
-      res.render( 'room', {
-          title : 'Comet Todo list',
-          id: req.params.room,
-          todos : todos
+      res.render("room", {
+        title: "Comet Todo list",
+        id: req.params.room,
+        todos: todos
       });
     });
 };
 
-exports.create = function ( req, res, next ){
+exports.create = function(req, res, next) {
   //res.clearCookie('room_id');
+  console.log(1);
   var uid = utils.uid(10);
-  console.log('uid = ' + uid)
-  
+  console.log("uid = " + uid);
+  console.log(2);
   new Hashlist({
-    hash_id    : uid,
-    enable     : true,
-    updated_at : Date.now()
-  }).save( function ( err, list, count ){
-    if( err ) res.redirect('/');
-
-    res.redirect('/room/' + uid);
+    hash_id: uid,
+    enable: true,
+    updated_at: Date.now()
+  }).save(function(err, list, count) {
+    console.log(3);
+    if (err) res.redirect("/");
+    console.log(4);
+    res.redirect("/room/" + uid);
   });
-  
 };
 
-exports.current_room = function ( req, res, next ){
-  console.log('aaaaaaaaa');
-  if (!req.params.hasOwnProperty('room')) {
-    res.render('404', {title: 'Error 404'});
+exports.current_room = function(req, res, next) {
+  if (!req.params.hasOwnProperty("room")) {
+    res.render("404", { title: "Error 404" });
     return;
   }
   var uid = req.params.room;
-  console.log(uid)
   Hashlist.find({
-      hash_id: uid       
-  }).
-  run(function (err, list) {
+    hash_id: uid
+  }).run(function(err, list) {
     if (err) {
-      console.log('bbbbbb')
-      res.render('404', {title: 'Error 404'});
+      res.render("404", { title: "Error 404" });
       return;
     }
-    
+
     //res.cookie( 'room_id', uid, {path: '/'});
     next();
-          
   });
-
 };
 
 //  User Logout & clear session.
-exports.logout = function ( req, res, next ){
-    req.session.destroy();
-    res.redirect( '/' );
-}
-
+exports.logout = function(req, res, next) {
+  req.session.destroy();
+  res.redirect("/");
+};
